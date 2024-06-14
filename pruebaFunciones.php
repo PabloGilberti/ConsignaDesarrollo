a51010d919c6c678006d5c1c449a03bb7d47ce2e
PabloGilberti <pgilberti@gmail.com>
2024-06-14 19:02:04 -0300

    Se agrega funciones MostrarProducto y ActualizarProducto

--- pruebaFunciones.php
+++ pruebaFunciones.php
@@ -19,3 +19,21 @@ function buscarProductoPorNombreModelo($productos, $nombre,$modelo) {
     }
     return "Producto no encontrado.<br>";
 }
+function mostrarProductos($productos) {
+    $result = '';
+    foreach ($productos as $producto) {
+        $result .= "Nombre: " . $producto['nombre'] . ", Precio: $" . $producto['precio'] . ", Cantidad: " . $producto['cantidad'] . "<br>";
+    }
+    return $result;
+}
+
+function actualizarProducto($productos, $nombre,$modelo, $precio, $cantidad) {
+    foreach ($productos as &$producto) {
+        if ($producto['nombre'] == $nombre || $producto['modelo'] == $modelo) {
+            $producto['precio'] = $precio;
+            $producto['cantidad'] = $cantidad;
+            break;
+        }
+    }
+    return $productos;
+}
