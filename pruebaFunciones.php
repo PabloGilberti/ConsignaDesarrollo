37f6382700c5960acd5da6e442046431b33d3d4d
PabloGilberti <pgilberti@gmail.com>
2024-06-14 19:18:37 -0300

    Se modifica variables del Modelo

--- pruebaFunciones.php
+++ pruebaFunciones.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 
 // Definir las funciones
 function agregarProducto($productos, $nombre,$modelo, $precio, $cantidad) {
@@ -37,3 +36,52 @@ function actualizarProducto($productos, $nombre,$modelo, $precio, $cantidad) {
     }
     return $productos;
 }
+// Inicializar el array de productos en la sesión
+if (!isset($_SESSION['productos'])) {
+    $_SESSION['productos'] = [];
+}
+
+$productos = $_SESSION['productos'];
+$resultado = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $accion = $_POST['accion'];
+    $nombre = $_POST['nombre'] ?? '';
+    $precio = $_POST['precio'] ?? '';
+    $cantidad = $_POST['cantidad'] ?? '';
+
+    switch ($accion) {
+        case 'agregar':
+            $productos = agregarProducto($productos, $nombre,$modelo, $precio, $cantidad);
+            $resultado = "Producto agregado correctamente.<br>";
+            break;
+        
+        case 'buscar':
+            $resultado = buscarProductoPorNombre($productos, $nombre);
+            break;
+        
+        case 'mostrar':
+            $resultado = mostrarProductos($productos);
+            break;
+        
+        case 'actualizar':
+            $productos = actualizarProducto($productos, $nombre, $precio, $cantidad);
+            $resultado = "Producto actualizado correctamente.<br>";
+            break;
+
+        case 'limpiar':
+            $_SESSION['productos'] = [];
+            $resultado = "Resultados limpiados correctamente.<br>";
+            session_destroy();
+            break;
+
+        default:
+            $resultado = "Acción no válida.";
+    }
+
+    $_SESSION['productos'] = $productos;
+    $_SESSION['resultado'] = $resultado;
+}
+
+
+?>
