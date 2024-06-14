76e025bc081eb30dcade6cee1c5b6f50c519dfd7
PabloGilberti <pgilberti@gmail.com>
2024-06-14 18:54:43 -0300

    Se crea la funcion buscarProductoPorNombre y agregarProducto

---
+++ pruebaFunciones.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+// Definir las funciones
+function agregarProducto($productos, $nombre,$modelo, $precio, $cantidad) {
+    $productos[] = [
+        'nombre' => $nombre,
+        'modelo' => $modelo,
+        'precio' => $precio,
+        'cantidad' => $cantidad
+    ];
+    return $productos;
+}
+function buscarProductoPorNombre($productos, $nombre,$modelo) {
+    foreach ($productos as $producto) {
+        if ($producto['nombre'] == $nombre || $producto['modelo'] == $modelo) {
+            return "Precio: $" . $producto['precio'] . ", Cantidad: " . $producto['cantidad'] . "<br>";
+        }
+    }
+    return "Producto no encontrado.<br>";
+}
